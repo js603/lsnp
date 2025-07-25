3793419ae5e06b334aa7bb4cb832c40f780064a9
js603 <skt1541@gmail.com>
2025-07-26 02:04:53 +0900

    new ready

--- src/TitleScreen.js
+++
@@ -1,4 +0,0 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import { useGame } from '../../contexts/GameContext';
-import GameContainer from '../game/GameContainer';
--- src/components/screens/GameScreen.js
+++
@@ -1,6 +0,0 @@
-import React, { useState, useEffect, useRef } from 'react';
-import styled from 'styled-components';
-import { useGame } from '../../contexts/GameContext';
-import GameContainer from '../game/GameContainer';
-import ChoiceButton from '../ui/ChoiceButton';
-import CharacterPortrait from '../ui/CharacterPortrait';
